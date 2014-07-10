c6b4584bfc0a403059b665cad018eec843390480
Victor <viktorsnt@gmail.com>
2014-07-10 18:10:03 -0300

    .markdown ignored on middleman build

--- config.rb
+++ config.rb
@@ -70,6 +70,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+ignore '*.markdown'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
